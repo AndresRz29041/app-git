0f41dcef5ada66188bacc59bae6bb51d92ab8fc0
andres <andres.andybeto@gmail.com>
2025-11-10 22:58:35 -0600

    agregando archivos de negocio y testing

---
+++ AppNegocio/Negocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNegocio
+{
+    public class Negocio
+    {
+
+        public double CalcularY(double x) {
+            double a = 2;
+            double b = 2;
+            double c = 2;
+            double y = 0;
+
+            try
+            {
+                y = ((a) * Math.Pow(x, 2)) + b * (x) + c;
+                return y;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error obteniendo x");
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ AppTesting/CalcularTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using AppNegocio;
+
+namespace AppTesting
+{
+    [TestClass]
+    public class CalcularTest
+    {
+        [TestMethod]
+        public void TestCalcularYExitoso()
+        {
+
+            double x = 2;
+            double y = 0;
+            double esperado = 14;
+
+            Negocio Negocio = new Negocio();
+
+            y = Negocio.CalcularY(x);
+
+            Assert.AreEqual(esperado, y);  
+        }
+
+    }
+}
